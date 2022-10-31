345720a8eb9d815e78fbb8c539cd12d0ff0ab712
Juan Carlos Medina <cixcsistemas@costadelsolperu.com>
2022-10-31 17:53:17 -0500

    actualizacion31-10

--- ProyectoDjango/ProyectoDjango/settings.py
+++ ProyectoDjango/ProyectoDjango/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
-    'pages'
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'ProyectoDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'psql',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- ProyectoDjango/pages/admin.py
+++ ProyectoDjango/pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+admin.site.register(Page)
+
+title="Proyecto con DJANGO"
+subtitle="Panel de Gestion"
+
+admin.site.site_header= title
+admin.site.site_title= title
+admin.site.index_title= subtitle
