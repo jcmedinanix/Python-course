535c8e125b588d4dbb1d19a07aec2fdedabfa2d8
Juan Carlos Medina <cixcsistemas@costadelsolperu.com>
2022-11-18 17:03:56 -0500

    actualizacion18-11

--- ProyectoDjango/blog/views.py
+++ ProyectoDjango/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import Category, Article
 
 # Create your views here.
@@ -10,4 +10,26 @@ def list(request):
     return render(request,'articles/list.html',{
         'title':'Articulos',
         'articles':articles
+    })
+
+def category(request, category_id):
+
+    #category=Category.objects.get(id=category_id)
+    category=get_object_or_404(Category,id=category_id)
+    #articles=Article.objects.filter(categories=category_id)
+
+    return render(request,'categories/category.html',{
+        'category':category,
+      #  'articles': articles
+
+    })
+
+
+def article(request,article_id):
+
+    article=get_object_or_404(Article,id=article_id)
+
+    return render(request,'articles/detail.html',{
+        'article':article
+
     })
